d7d2a03ff9e0f1bbb4a352a2a9f46d019cfcff5e
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-28 12:19:41 -0300

    feat: fix bug NaN and Finish the Project

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,74 +4,48 @@ import Button from '../components/Button';
 import Display from '../components/Display';
 
 const initialState = {
-    displayValue: '0',  
-    clearDisplay: false, 
-    operation: null,     
-    values: [0, 0],      
-    current: 0        
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0 , 0],
+    current: 0
 };
 
 export default class Calculator extends Component {
-    state = { ...initialState };
+    state = { ...initialState }
 
-    constructor(props) {
+    constructor(props){
         super(props);
         this.clearMemory = this.clearMemory.bind(this);
         this.setOperation = this.setOperation.bind(this);
         this.addDigit = this.addDigit.bind(this);
+        this.calculate = this.calculate.bind(this);
     }
 
     clearMemory() {
-        this.setState({ ...initialState });
+       this.setState({ ...initialState });
     }
 
     setOperation(operation) {
         if (this.state.current === 0) {
             this.setState({
                 operation,
-                current: 1, 
-                clearDisplay: true  
+                current: 1,
+                clearDisplay: true
             });
         } else {
-            const equals = operation === '=';
-            const values = [...this.state.values];
-
-          
-            try {
-                values[0] = this.performOperation(values[0], values[1], this.state.operation);
-            } catch (e) {
-                values[0] = this.state.values[0];  
-            }
-
-            values[1] = 0;  
+            this.calculate();
             this.setState({
-                displayValue: values[0].toString(),
-                operation: equals ? null : operation,
-                current: equals ? 0 : 1,
-                clearDisplay: !equals, 
-                values
+                operation,
+                current: 1,
+                clearDisplay: true
             });
-        }
-    }
-
-    performOperation(value1, value2, operation) {
-        switch (operation) {
-            case '+':
-                return value1 + value2;
-            case '-':
-                return value1 - value2;
-            case '*':
-                return value1 * value2;
-            case '/':
-                return value1 / value2;
-            default:
-                return value2;
         }
     }
 
     addDigit(n) {
         if (n === '.' && this.state.displayValue.includes('.')) {
-            return;
+            return; 
         }
 
         const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
@@ -79,13 +53,44 @@ export default class Calculator extends Component {
         const displayValue = currentValue + n;
 
         this.setState({ displayValue, clearDisplay: false });
+    }
 
-        if (n !== '.') {
-            const i = this.state.current;
-            const newValue = parseFloat(displayValue);
-            const values = [...this.state.values];
-            values[i] = newValue;
-            this.setState({ values });
+    calculate() {
+        const [first, second] = this.state.values;
+        const { operation } = this.state;
+
+        if (operation === '+') {
+            this.setState({
+                displayValue: (first + second).toString(),
+                operation: null,
+                current: 0
+            });
+        } else if (operation === '-') {
+            this.setState({
+                displayValue: (first - second).toString(),
+                operation: null,
+                current: 0
+            });
+        } else if (operation === '*') {
+            this.setState({
+                displayValue: (first * second).toString(),
+                operation: null,
+                current: 0
+            });
+        } else if (operation === '/') {
+            if (second === 0) {
+                this.setState({
+                    displayValue: 'Error',
+                    operation: null,
+                    current: 0
+                });
+            } else {
+                this.setState({
+                    displayValue: (first / second).toString(),
+                    operation: null,
+                    current: 0
+                });
+            }
         }
     }
 
@@ -94,22 +99,22 @@ export default class Calculator extends Component {
             <div className="calculator">
                 <Display value={this.state.displayValue} />
                 <Button label="AC" className="clear" onClick={this.clearMemory} />
-                <Button label="/" className="operator" onClick={this.setOperation} />
-                <Button label="7" className="num" onClick={this.addDigit} />
-                <Button label="8" className="num" onClick={this.addDigit} />
-                <Button label="9" className="num" onClick={this.addDigit} />
-                <Button label="*" className="operator" onClick={this.setOperation} />
-                <Button label="4" className="num" onClick={this.addDigit} />
-                <Button label="5" className="num" onClick={this.addDigit} />
-                <Button label="6" className="num" onClick={this.addDigit} />
-                <Button label="-" className="operator" onClick={this.setOperation} />
-                <Button label="1" className="num" onClick={this.addDigit} />
-                <Button label="2" className="num" onClick={this.addDigit} />
-                <Button label="3" className="num" onClick={this.addDigit} />
-                <Button label="+" className="operator" onClick={this.setOperation} />
-                <Button label="0" className="wide num" onClick={this.addDigit} />
-                <Button label="." className="num" onClick={this.addDigit} />
-                <Button label="=" className="equal" onClick={this.setOperation} />
+                <Button label="/" className="operator" onClick={() => this.setOperation('/')} />
+                <Button label="7" className="num" onClick={() => this.addDigit('7')} />
+                <Button label="8" className="num" onClick={() => this.addDigit('8')} />
+                <Button label="9" className="num" onClick={() => this.addDigit('9')} />
+                <Button label="*" className="operator" onClick={() => this.setOperation('*')} />
+                <Button label="4" className="num" onClick={() => this.addDigit('4')} />
+                <Button label="5" className="num" onClick={() => this.addDigit('5')} />
+                <Button label="6" className="num" onClick={() => this.addDigit('6')} />
+                <Button label="-" className="operator" onClick={() => this.setOperation('-')} />
+                <Button label="1" className="num" onClick={() => this.addDigit('1')} />
+                <Button label="2" className="num" onClick={() => this.addDigit('2')} />
+                <Button label="3" className="num" onClick={() => this.addDigit('3')} />
+                <Button label="+" className="operator" onClick={() => this.setOperation('+')} />
+                <Button label="0" className="wide num" onClick={() => this.addDigit('0')} />
+                <Button label="." className="num" onClick={() => this.addDigit('.')} />
+                <Button label="=" className="equal" onClick={this.calculate} />
             </div>
         );
     }
